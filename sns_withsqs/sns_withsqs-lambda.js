2623b78245c0677eea34b5507abc2a3be9e84df0
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-16 16:23:43 +0530

    sns added to publish to existing sqs

--- sns_withsqs/sns_withsqs-lambda.js
+++ sns_withsqs/sns_withsqs-lambda.js
@@ -1,7 +1,39 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
 
-	callback(null,'Successfully executed');
+	callback(null, 'Successfully executed');
 	console.log("succesfully triggered");
+
+	sns.publish({
+		Message: 'Message Body for the SQS',
+		Subject: 'Subject_MessagetoSQS',
+		MessageAttributes: {
+			'name': {
+				DataType: 'String',
+				StringValue: 'fname'
+			},
+			'class': {
+				DataType: 'String',
+				StringValue: 'class2'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:New_upuliesqs'
+	}).promise()
+		.then(data => {
+			// your code goes here
+       
+	    console.log("successfully published to sqs",data);
+
+		})
+		.catch(err => {
+			// error handling goes here
+
+		console.log("Error encountered" ,err );	
+		});
+
+
+
 }
