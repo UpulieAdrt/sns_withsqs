365a4b248f21a9398926c9829c48ecea3741ea32
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-16 17:36:39 +0530

    revert back to trigger only state

--- sns_withsqs/sns_withsqs-lambda.js
+++ sns_withsqs/sns_withsqs-lambda.js
@@ -6,34 +6,6 @@ exports.handler = function (event, context, callback) {
 	callback(null, 'Successfully executed');
 	console.log("succesfully triggered");
 
-	sns.publish({
-		Message: 'Message Body for the SQS',
-		Subject: 'Subject_MessagetoSQS',
-		MessageAttributes: {
-			'name': {
-				DataType: 'String',
-				StringValue: 'fname'
-			},
-			'class': {
-				DataType: 'String',
-				StringValue: 'class2'
-			},
-		},
-		MessageStructure: 'String',
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:New_upuliesqs'
-	}).promise()
-		.then(data => {
-			// your code goes here
-       
-	    console.log("successfully published to sqs",data);
-
-		})
-		.catch(err => {
-			// error handling goes here
-
-		console.log("Error encountered" ,err );	
-		});
-
 
 
 }
