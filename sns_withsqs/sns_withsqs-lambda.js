234ef0bb986b43bf470d2e608e5e91d9c22c03ba
UpulieAdrt <35255575+UpulieAdrt@users.noreply.github.com>
2018-01-16 17:50:18 +0530

    sns added to publish to sqs

--- sns_withsqs/sns_withsqs-lambda.js
+++ sns_withsqs/sns_withsqs-lambda.js
@@ -5,6 +5,32 @@ exports.handler = function (event, context, callback) {
 
 	callback(null, 'Successfully executed');
 	console.log("succesfully triggered");
+	sns.publish({
+		Message: 'This is the message for SQS',
+		Subject: 'SQS Message subject',
+		MessageAttributes: {
+			'name': {
+				DataType: 'String',
+				StringValue: 'sam'
+			},
+			'class': {
+				DataType: 'String',
+				StringValue: 'class2'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_topicU'
+	}).promise()
+		.then(data => {
+			// your code goes here
+
+			console.log("Published to sqs" ,data)
+		})
+		.catch(err => {
+			// error handling goes here
+
+			console.log("Error" , err);
+		});
 
 
 
